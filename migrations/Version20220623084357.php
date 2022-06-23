67517b1189fd39d4c926a2a25bdb5ffd93ce0f09
Nicolas Vauche <hello@nicolasvauche.net>
2022-06-23 08:51:16 +0000

    Fix Migrations

--- migrations/Version20220623084357.php
+++ migrations/Version20220623084357.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220621074258 extends AbstractMigration
+final class Version20220623084357 extends AbstractMigration
 {
     public function getDescription(): string
     {
